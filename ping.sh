2b82be4be1ce1438214677ec0ac3e0ab923da383
gitlexander <astavro123@gmail.com>
2024-10-22 15:45:06 -0400

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-curl -s http://localhost:8000 > /dev/null
+while true
+do
+    curl -s https://calc1-6nke.onrender.com/subtasks > /dev/null
+    echo "Pinged https://calc1-6nke.onrender.com/subtasks"
+    sleep 300  # Sleep for 5 minutes
+done
